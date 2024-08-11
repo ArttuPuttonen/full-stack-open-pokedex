9190a91e27dbe9c03c3dc05ea126c7e1540f0a34
Arttu Puttonen <arskaputtonen@gmail.com>
2024-08-11 21:17:42 +0300

    11.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Routes, Route, useMatch } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom' // Use Routes, not BrowserRouter as Routes
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-const mapResults = (({ results }) => results.map(({ url, name }) => ({
+const mapResults = ({ results }) => results.map(({ url, name }) => ({
   url,
   name,
   id: parseInt(url.match(/\/(\d+)\//)[1])
-})))
+}))
 
 const App = () => {
   const match = useMatch('/pokemon/:name')
