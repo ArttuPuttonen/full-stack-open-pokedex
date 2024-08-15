266934a7ad73219e731665c85b9c96366b682e91
Arttu Puttonen <arskaputtonen@gmail.com>
2024-08-15 12:38:47 +0300

    updated node

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   roots: ['<rootDir>/test/jest'],
   testMatch: ['**/?(*.)+(spec|test).js?(x)'],
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/test/playwright/'],
 }
