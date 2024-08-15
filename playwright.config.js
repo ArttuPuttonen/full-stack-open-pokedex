ba392d26496cbc00baba6b7529807a81996a3ba3
Arttu Puttonen <arskaputtonen@gmail.com>
2024-08-15 11:46:05 +0300

    playwright fixed?

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@playwright/test')
 
 module.exports = defineConfig({
-  testDir: './test/playwright', // Directory where your Playwright tests are located
+  testDir: './test/playwright',
   timeout: 30000,
   retries: 1,
   reporter: [['list'], ['json', { outputFile: 'test-results.json' }]],
@@ -11,13 +11,13 @@ module.exports = defineConfig({
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'on-first-retry',
-    baseURL: 'http://localhost:3000', // Assuming your app runs on port 3000
+    baseURL: 'http://localhost:8081',
   },
 
   webServer: {
-    command: 'npm run start', // This command starts your development server
-    port: 3000, // Port where your app runs
-    timeout: 120 * 1000, // Wait for up to 120 seconds for the server to start
-    reuseExistingServer: !process.env.CI, // Do not start a new server if one is already running
+    command: 'npm run start',
+    port: 8081,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
 })
