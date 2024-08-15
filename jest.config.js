7c0d0063a08020c4162870c345916356411ddd10
Arttu Puttonen <arskaputtonen@gmail.com>
2024-08-15 12:50:45 +0300

    noh

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>/test/jest'],
-  testMatch: ['**/?(*.)+(spec|test).js?(x)'],
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/test/playwright/'],
-}
