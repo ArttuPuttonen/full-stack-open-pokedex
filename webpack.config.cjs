c54e03b3e9428b5dae6e28166049f98011b584e8
Arttu Puttonen <arskaputtonen@gmail.com>
2024-08-15 14:56:36 +0300

    trying out

---
+++ webpack.config.cjs
@@ -0,0 +1,53 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.jsx',  // Your main entry point
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,  // Regex to match both .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',  // Use Babel loader for transpiling
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],  // Add .jsx to the extensions Webpack resolves
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+}
