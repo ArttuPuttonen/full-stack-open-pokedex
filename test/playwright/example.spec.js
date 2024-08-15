0737b6f2e30ee7462edb876cf1da19a4c5f7fa77
Arttu Puttonen <arskaputtonen@gmail.com>
2024-08-15 21:13:34 +0300

    url added to the play test

--- test/playwright/example.spec.js
+++ test/playwright/example.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('')
+    await page.goto('http://localhost:5001')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
